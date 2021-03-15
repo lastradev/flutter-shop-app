2b3a2f72df87638da4f51cf9694ef372b57cbf29
Oscar Lastra <oscar.glastra@gmail.com>
2021-03-15 11:57:26 -0600

    Provide title from data

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -41,6 +41,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((product) => product.id == id);
+  }
+
   void addProduct() {
     // _items.add(value);
     notifyListeners();
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId =
-        ModalRoute.of(context)!.settings.arguments as String; // is the id
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
