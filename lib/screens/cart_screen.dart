adac6c886d2c0cac1451893b15e8b3825951b115
Oscar Lastra <oscar.glastra@gmail.com>
2021-04-02 02:29:50 -0600

    Display list of cart items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import '../providers/cart.dart';
 import 'package:provider/provider.dart';
+
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -42,6 +44,18 @@ class CartScreen extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItem(
+                id: cart.items.values.toList()[i].id,
+                price: cart.items.values.toList()[i].price,
+                quantity: cart.items.values.toList()[i].quantity,
+                title: cart.items.values.toList()[i].title,
               ),
             ),
           ),
---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  final String id;
+  final double price;
+  final int quantity;
+  final String title;
+
+  const CartItem({
+    required this.id,
+    required this.price,
+    required this.quantity,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text(
+                  '\$${price}',
+                ),
+              ),
+            ),
+          ),
+          title: Text(title),
+          subtitle: Text('Total: \$${price * quantity}'),
+          trailing: Text('$quantity x'),
+        ),
+      ),
+    );
+  }
+}
